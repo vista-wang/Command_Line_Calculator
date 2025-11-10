ac04881eebd14e4282fb41b5827d948aa3d4538a
vista-wang <156204219+vista-wang@users.noreply.github.com>
2025-11-10 18:10:24 +0800

    一个基础的框架，之后在switch里做指令的判断就行了， 这个程序暂时没有发现Bug

--- Command_Line_Calculator/main.swift
+++ Command_Line_Calculator/main.swift
@@ -6,6 +6,21 @@
 //
 
 import Foundation
+//import Cocoa
 
-print("Hello, World!")
+func main(){
+    print("calculator", terminator:">")
+    let command:String! = readLine()
+    print("\(command ?? "incorrect command")")
+    switch command {
+        case "quit":
+            break
+        default:
+            main()
+    }
+}
+
+let version = "InDev"
+print("Welcome to use Command Line Calculator(version \(version)! \n")
+main()
 
