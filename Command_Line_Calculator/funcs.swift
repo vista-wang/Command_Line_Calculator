11842b141764d7f7ef5df830a4af9dc1bafcd6d9
vista-wang <vista_128@outlook.com>
2025-11-13 21:58:31 +0800

    格式化输入程序OK了，把a从Str=>Int的代码还需要改一改（main.swift第18行）

---
+++ Command_Line_Calculator/funcs.swift
@@ -0,0 +1,14 @@
+//
+//  funcs.swift
+//  Command_Line_Calculator
+//
+//  Created by Ethan Wang    on 11/13/25.
+//
+
+func prt(prt:String){
+    
+}
+
+func set(a:Int, b:String){
+    
+}
--- Command_Line_Calculator/main.swift
+++ Command_Line_Calculator/main.swift
@@ -8,29 +8,53 @@
 import Foundation
 //import Cocoa
 
-func command_switch(command:String){
+func command_switch(command:String, a:String, b:String){
+    if Int(a) != nil{
+        
+    }
     switch command {
-    case "quit":
-        break
-    default:
-        main()
+        case "set":
+            let ai:Int! = Int(a)
+            set(a:ai, b:b)
+        case "quit":
+            break
+        default:
+            main()
     }
 }
 
 func main(){
     print("calculator", terminator:">")
-    let command:String! = readLine()
-    print("\(command ?? "incorrect command")")
-    /*switch command {
+    let input:String! = readLine()
+    /*print("\(command ?? "incorrect command")")
+    switch command {
         case "quit":
             break
         default:
             main()
     }*/
-    command_switch(command:command)
+    let part = input.components(separatedBy: " ")
+    if part.count == 3{
+        let command = part[0]
+        let a = part[1]
+        let b = part[2]
+        command_switch(command:command, a:a, b:b)
+    }else if part.count == 2{
+        let command = part[1]
+        if command == "PRT"{
+            prt(prt:part[2])
+        }else{
+            print("less input")
+        }
+    }else{
+        print("incorrect input")
+    }
 }
 
 let version = "InDev"
-print("Welcome to use Command Line Calculator(version \(version)! \n")
+//let command:String
+//let a:String
+//let b:String
+print("Welcome to use Command Line Calculator(version \(version)）! \n")
 main()
 
